3f4b7b06845f278f0759f666dd513461f609386f
Nurulhusna111305 <105391520+Nurulhusna111305@users.noreply.github.com>
2022-05-12 05:39:34 +0700

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C/DTDXHTML10 transit
+www.w3.org/TR/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Modularitas</title>
+</head>
+
+<body>
+<a href="index.php">Home</a> |
+<a href="about.php">About Us</a> |
+<a href="link.php">Links</a> |
+<a href="contact.php">Contact</a> |
+<a href="new.php">New Menu</a> |
+<a href="Include">Include</a> |
+<a href="require.php">Require</a> |
+<a href="perbedaan.php">Perbedaan Include dan Require</a> |
+</body>
+</html>
